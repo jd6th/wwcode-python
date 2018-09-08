ce2ddb1ff1daf09ad3824c312df9b2e8120e679f
jd6th <jd6th@yahoo.com>
2018-09-08 14:38:17 +0800

    change variables

--- challenge_set_reward.py
+++ challenge_set_reward.py
@@ -4,12 +4,24 @@ Created on Sat Sep  8 14:10:07 2018
 
 @author: Majoy
 """
-
 txt = input('Please enter text: ')
-reward = "coke sakto"
+
 num = txt[0]
 txt = txt[:-1]
 txt = txt[1:]
 txt = txt.strip()
 
-print('Hi {}! You have successfully redeem reward #{}-{}! Thank you for choosing Aling Nena’s Sari-sari store.'.format(txt.title(), num, reward))
+
+rewards = [
+    "Coke sakto",  # index 0
+    "Boy Bawang",  # index 1
+    "15pcs. Yucky candy",  # index 2
+    "15pcs. Pintura candy",  # index 3
+    "15PHP load",  # index 4
+    "3 pcs. Dove conditioner",  # index 5
+    "Cottonbuds",  # index 6
+    "One sheet of Biogesic",  # index 7
+    "100mL Pepper/Pintura",  # index 8
+]
+
+print('Hi {}! You have successfully redeem reward #{}-{}! Thank you for choosing Aling Nena’s Sari-sari store.'.format(txt.title(), num, rewards[int(num)]))
